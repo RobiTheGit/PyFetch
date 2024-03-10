66255dd2fcbb0757f30dcebde8982c422f89d5ac
RobiWanKenobi <94720060+RobiTheGit@users.noreply.github.com>
2024-03-10 17:52:20 -0600

    changed some formatting

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 import os
 import psutil
 import colors
+
 print(colors.white)
 OS_Release = ((open('/etc/os-release', 'r').readline().split('=')[1]).replace('"', '')).replace('\n', '')
 
@@ -10,11 +11,10 @@ uptime = (open('/proc/uptime', 'r').readline().split(' '))[0]
 CPU_Model_Name = ((((open('/proc/cpuinfo', 'r').readlines()[4].split(':'))[1]).replace('\n', '')).split(' ', 1))[1]
 
 mem = psutil.virtual_memory()
-print(os.getlogin() + '@' + os.uname().nodename, colors.white)
+print(colors.red, os.getlogin() + colors.white + '@' + colors.red +os.uname().nodename, colors.white)
 print(colors.red, 'OS:', '\t', '\t', colors.white, OS_Release, os.uname().machine, colors.white)
 print(colors.red, f'Shell:','\t', colors.white, str(os.environ.get("SHELL")).replace('/bin/',''), colors.white)
-print(colors.red, 'CPU:', '\t', '\t', colors.white, CPU_Model_Name, colors.white)
-print(colors.red, 'CPU Cores:', '\t', colors.white, os.cpu_count(), colors.white)
+print(colors.red, 'CPU:', '\t', '\t', colors.white, CPU_Model_Name, f'({os.cpu_count()})', colors.white)
 print(colors.red, 'Kernel:', '\t', colors.white, os.uname().release, colors.white)
 print(colors.red, 'Memory:', '\t', colors.white, round((int(mem.active) / 1.074e+9),2), 'GB', '/', round((int(mem.total) / 1.074e+9),2), 'GB', colors.white)
 print(colors.red, 'Uptime:', '\t', colors.white, round(((float(uptime) / 60)),2), 'Minutes', colors.white)
