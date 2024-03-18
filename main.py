177ce52d5436afc631b2b661eeb51def07c21771
RobiWanKenobi <94720060+RobiTheGit@users.noreply.github.com>
2024-03-18 14:34:35 -0600

    changes
    redid the colors system so it works better outside of dark mode
    changed how the information is printed out

--- main.py
+++ main.py
@@ -6,28 +6,56 @@ import sys
 import os
 import psutil
 import colors
+'''
+Variable Setup
+'''
 gpu1 = (((os.popen('lspci -nn | grep -E "Display|3D|VGA"').read()).split(':',2)[2]).split('[',2)[0]).split(' ', 1)[1]
 gpu2 = (((os.popen('lspci -nn | grep -E "Display|3D|VGA"').read()).split(':',2)[2]).split('[',2)[1]).replace(']', '')
 GPU_Pretty = gpu1 + gpu2
-print(colors.white)
 OS_Release = ((open('/etc/os-release', 'r').readline().split('=')[1]).replace('"', '')).replace('\n', '')
 uptime = (open('/proc/uptime', 'r').readline().split(' '))[0]
 CPU_Model_Name = ((((open('/proc/cpuinfo', 'r').readlines()[4].split(':'))[1]).replace('\n', '')).split(' ', 1))[1]
 Monitor_Width = ((open(f'{os.getenv("HOME")}/.config/monitors.xml', 'r').readlines()[18]).replace('<','').replace('>','').replace('/','').replace('\n', '').replace(' ', '').replace('width', ''))
 Monitor_Height = ((open(f'{os.getenv("HOME")}/.config/monitors.xml', 'r').readlines()[19]).replace('<','').replace('>','').replace('/','').replace('\n', '').replace(' ', '').replace('height', ''))
 mem = psutil.virtual_memory()
-print(colors.red, os.getlogin() + colors.white + '@' + colors.red +os.uname().nodename, colors.white)
-print(colors.red, 'OS:', '\t', '\t', colors.white, OS_Release, os.uname().machine, colors.white)
-print(colors.red, f'Shell:','\t', colors.white, str(os.environ.get("SHELL")).replace('/bin/','').title(), colors.white)
-print(colors.red, 'Model:', '\t',colors.white, (open('/sys/devices/virtual/dmi/id/product_name','r').readline()).replace('\n',''),colors.white)
-print(colors.red, 'Vendor:', '\t',colors.white, (open('/sys/devices/virtual/dmi/id/sys_vendor','r').readline()).replace('\n',''),colors.white)
-print(colors.red, 'CPU:', '\t', '\t', colors.white, CPU_Model_Name, f'({os.cpu_count()})', colors.white)
-print(colors.red, 'Kernel:', '\t', colors.white, os.uname().release, colors.white)
-print(colors.red, 'Memory:', '\t', colors.white, round((int(mem.active) / 1.074e+9),2), 'GB', '/', round((int(mem.total) / 1.074e+9),2), 'GB', colors.white)
-print(colors.red, 'Uptime:', '\t', colors.white, round(((float(uptime) / 60)),2), 'Minutes', colors.white)
-print(colors.red, f'Desktop:', '\t',  colors.white, os.environ.get("XDG_CURRENT_DESKTOP"), colors.white)
-print(colors.red, f'Display Server:', colors.white, os.environ.get("XDG_SESSION_TYPE").title(), colors.white)
-print(colors.red, f'Cursor Theme:  ', colors.white, os.environ.get("XCURSOR_THEME"), colors.white)
-print(colors.red, f'Resolution:', '\t',colors.white,f'{Monitor_Width}x{Monitor_Height}' , colors.white)
-print(colors.red, f'GPU:'+ '\t'+ '\t',colors.white, GPU_Pretty, colors.white)
-print(colors.disptest)
+shellstr = str(os.environ.get("SHELL")).replace('/bin/','').title()
+modelstr = (open('/sys/devices/virtual/dmi/id/product_name','r').readline()).replace('\n','')
+User = f'{colors.red} {os.getlogin()}{colors.none}@{colors.red}{os.uname().nodename}{colors.none}'
+vendorstr = (open('/sys/devices/virtual/dmi/id/sys_vendor','r').readline()).replace('\n','')
+RAMstr = f"{colors.none}{round((int(mem.active) / 1.074e+9),2)} GB / {round((int(mem.total) / 1.074e+9),2)} GB"
+Desktopstr = os.environ.get("XDG_CURRENT_DESKTOP")
+DispServStr = os.environ.get("XDG_SESSION_TYPE").title()
+CurStr = os.environ.get("XCURSOR_THEME")
+
+OS = f'{colors.red} OS:\t\t{colors.none} {OS_Release} {os.uname().machine}'
+Shell =f'{colors.red} Shell:\t\t{colors.none} {shellstr}'
+Model = f'{colors.red} Model:\t\t{colors.none} {modelstr}'
+Vendor = f'{colors.red} Vendor:\t{colors.none} {vendorstr} '
+CPU = f'{colors.red} CPU: \t\t{colors.none} {CPU_Model_Name} {os.cpu_count()}'
+Kernel = f'{colors.red} Kernel: \t{colors.none} {os.uname().release}'
+RAM = f'{colors.red} Memory: \t {RAMstr} {colors.none}'
+Uptime = f'{colors.red} Uptime: \t{colors.none} {round(((float(uptime) / 60)),2)} Minutes'
+Desktop = f'{colors.red} Desktop: \t {colors.none}{Desktopstr}'
+DispServ = f"{colors.red} Display Server:{colors.none} {DispServStr}"
+CursorTheme = f"{colors.red} Cursor Theme:  {colors.none} {CurStr}"
+Resolution = f"{colors.red} Resolution: \t{colors.none} {Monitor_Width}x{Monitor_Height}"
+GPU = f"{colors.red} GPU:\t\t {colors.none}{GPU_Pretty}"
+ColoredBlocks = (f'\n{colors.disptest}\n{colors.disptest}\n')
+
+print(f"""{colors.none}
+{User}
+{OS}
+{Shell}
+{Model}
+{Vendor}
+{CPU}
+{Kernel}
+{RAM}
+{Uptime}
+{Desktop}
+{DispServ}
+{CursorTheme}
+{Resolution}
+{GPU}
+{ColoredBlocks}
+      """)
